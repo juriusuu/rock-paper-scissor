37d4aa6ca8c328dabd49200b3697a13fecf2b410
“Juwiusu” <75914740+juriusuu@users.noreply.github.com>
2023-03-20 22:23:34 +0800

    added functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {}
+
+function playRound(playerSelection, computerSelection) {}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (let i = 0; i < 5; i++) {}
+}
